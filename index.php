0165524d721760982162fccd0785c108dd93bcb7
feb-dev <62356867+feb-dev@users.noreply.github.com>
2020-08-26 13:53:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require_once 'simple_html_dom.php';
 
 $source_url = 'https://check.torproject.org/';
 $html_source = file_get_html($source_url);
-echo 'IP : '. $ip = $html_source->find('<strong>', 0)->plaintext;
+$ip = $html_source->find('<strong>', 0)->plaintext;
 ?>
