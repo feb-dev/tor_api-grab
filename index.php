d0087a5345e3e0c95571ca7b4c2e4fd793892be2
feb-dev <62356867+feb-dev@users.noreply.github.com>
2020-08-26 13:49:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'simple_html_dom.php';
+
+$source_url = 'https://check.torproject.org/';
+$html_source = file_get_html($source_url);
+echo 'IP : '. $ip = $html_source->find('<strong>', 0)->plaintext;
+?>
